11ab21a8715c9a67d93fda9c459c4fb81da1ca6b
Frogblast <89352653+Frogblast@users.noreply.github.com>
2023-12-30 12:58:05 +0100

    Added handling of division by 0

--- calculator.cpp
+++ calculator.cpp
@@ -21,6 +21,7 @@ int main()
         switch (_operator)
         {
         case '+':
+            // stoi(string) stands for "string" to "int"
             result = stoi(operand1) + stoi(operand2);
             cout << "Sum is: " << result << endl;
             break;
@@ -33,11 +34,20 @@ int main()
             cout << "Product is: " << result << endl;
             break;
         case '/':
-            result = static_cast<float>(stoi(operand1)) / static_cast<float>(stoi(operand2));
-            cout << "Quotient is: " << result << endl;
+            if (operand2 == "0")
+            {
+                cout << "Can't divide by zero" << endl;
+            }
+            else
+            {
+                // static_cast<float> casts an int to a float. Combined with stoi it converts a string to a float
+                result = static_cast<float>(stoi(operand1)) / static_cast<float>(stoi(operand2));
+                cout << "Quotient is: " << result << endl;
+            }
             break;
 
         default:
+            cout << "Invalid operation" << endl;
             break;
         }
         cout << "Do you want to quit? y/n" << endl;
