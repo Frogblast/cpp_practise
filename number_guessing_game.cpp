b39e27dbf4a32f1f42e54f68acee6f90320db2c6
Frogblast <89352653+Frogblast@users.noreply.github.com>
2023-12-30 20:16:24 +0100

    Added comments and refactored the code

--- number_guessing_game.cpp
+++ number_guessing_game.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <stdlib.h>
-#include <ctime>
+#include <ctime> // For time(int) function
 
 using namespace std;
 
@@ -9,34 +8,58 @@ int main()
     // Declare variables
     bool running = true;
     int input;
+    unsigned int guessLimit;
+    unsigned int guessCounter = 0;
 
-    // Generate a random number
+    // Generate a random number between 1 and 100
     time_t randomSeed = time(0);
     srand(static_cast<unsigned int>(randomSeed));
     int secretNumber = randomSeed % 100 + 1;
 
     // Start game
+    cout << "Enter how many guesses you want (1-10)" << endl;
+    cin >> guessLimit;
+
+    // Limit guessLimits to 10
+    guessLimit > 10 ? guessLimit = 10 : guessLimit = guessLimit;
+
+    system("CLS"); // Clear screen. Part of iostream library
     cout << "There is a secret number between 1 and 100. Can you guess what it is?" << endl;
     while (running)
     {
+        // Check for game over
+        if (guessCounter >= guessLimit)
+        {
+            system("CLS");
+            cout << "No more guesses available. Game over!" << endl;
+            running = false;
+            continue;
+        }
+
+        cout << "\nGuesses remaining: " << guessLimit - guessCounter << endl;
         cin >> input;
 
         // Evaluates the guess
-        if (secretNumber == input)
+        if (input < secretNumber)
+        {
+            cout << "The secret number is bigger than the one you guessed. ";
+        }
+        else if (input > secretNumber)
+        {
+            cout << "The secret number is smaller than the one you guessed. ";
+        }
+        else
         {
             cout << "You found the secret number!" << endl;
             // End and quit the game
             running = false;
+            continue;
         }
-        else if (input > secretNumber)
-        {
-            cout << "The secret number is bigger than the one you guessed\n\t Guess again!" << endl;
-        }
-        else
-        {
-            cout << "The secret number is smaller than the one you guessed\n\t Guess again!" << endl;
-        }
+
+        guessCounter++;
+        cout << "Guess again!" << endl;
     }
+    cout << "The secret number was: " << secretNumber << endl;
 
     return 0;
 }
