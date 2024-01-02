11eecd5b0b5495d86759746db47d536c57d4f67b
Frogblast <89352653+Frogblast@users.noreply.github.com>
2024-01-02 22:36:13 +0100

    Added better comments and cleaned up the code

--- todo_list.cpp
+++ todo_list.cpp
@@ -2,27 +2,27 @@
 #include <vector>
 #include <fstream> // filestream, has datatype fstream that has the functions open(filepath, type of operation)
 #include <string>
+#include <conio.h> // for "getch() function"
 
 using namespace std;
 
 void viewList();
 bool addToList(string task);
-bool saveListToFile();
 void exit(bool *ptr);
 string txtPath = "resources/todo_list.txt";
 string newTask;
+void waitForUserInputToContinue();
 
-// instantiate a collection of some sort for strings that should be added to the list
+// Instantiate a collection of some sort for strings that should be added to the list
 vector<string> newTasks;
 fstream todoList;
 
 int main()
 {
     bool running{true};
-    // ask if user wants to see current list, add to the list or quit and save the changes
+    // While loop to ask if user wants to see current list, add to the list or quit and save the changes
     while (running)
     {
-
         cout << "Select an option:\n"
              << endl;
         cout << "View current list (1)\nAdd to the list (2)\nQuit and save changes (3)" << endl;
@@ -36,33 +36,37 @@ int main()
         case 2:
             cout << "Enter new task" << endl;
             cin.ignore();
-            getline(cin, newTask); // instead of "cin >> newTask" which only reads a word at a time with spaces as delimiters, use
-                                        //getline(istream, string) to get the whole sentence.
-            if(addToList(newTask)){
-                cout<<"Added: "<<newTask<<endl;
+            /*Instead of "cin >> newTask" which only reads a word at a time with spaces as delimiters,
+            use getline(istream, string) to get the whole sentence.
+            */
+            getline(cin, newTask);
+            if (addToList(newTask))
+            {
+                cout << "Added: " << newTask << endl;
             };
             break;
         case 3:
-            saveListToFile();
             exit(&running);
             break;
         default:
             cout << "Invalid choice" << endl;
             break;
         }
+        getch(); // Wait for user to hit enter (from library conio.h)
+        system("CLS");
     }
     return 0;
 }
 
-// show the list
+// Show the list
 void viewList()
 {
     string task;
-    todoList.open(txtPath, ios::in); // Parameters are the path and the mode of operation
+    // Parameters of the fstream-function "open" are the path of the text file and the mode of operation (ios::in for reading)
+    todoList.open(txtPath, ios::in);
     // Only operate if the fstream is open
     if (todoList.is_open())
     {
-        // while loop to access every line of text: access first line, store in variable and write out.
         cout << "List: " << endl;
         // Iterate through the fstream to access each line of the file
         while (getline(todoList, task))
@@ -74,14 +78,15 @@ void viewList()
     }
 }
 
-// add to the list
+// Add to the list, sending a true boolean value back if successful
 bool addToList(string task)
 {
-    todoList.open(txtPath, ios::app); //ios::app (append) to existing file. If it does not exist it creates a new.
-                                        //ios::out will only create a new file and overwrite the existing file.
+    todoList.open(txtPath, ios::app); // ios::app (append) to existing file. If it does not exist it creates a new.
+                                      // ios::out would create a new file, overwriting the existing one.
     if (todoList.is_open())
     {
-        todoList << task<<"\n";
+        // Uses data-stream to put the new task (string) in to the fstream object followed by a new line
+        todoList << task << "\n";
         todoList.close();
         return true;
     }
@@ -91,14 +96,8 @@ bool addToList(string task)
     }
 }
 
-// save list to file
-bool saveListToFile()
-{
-}
-
-// quit the program and save the changes
+// Quit the program and save the changes
 void exit(bool *ptr)
 {
-    saveListToFile();
     *ptr = false;
 }
