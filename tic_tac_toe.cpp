11c829cac46808ced8b2db38406ebebabc7124df
Frogblast <89352653+Frogblast@users.noreply.github.com>
2024-01-12 14:41:29 +0100

    Added tictactoe project

---
+++ tic_tac_toe.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main(){
+// clean up console
+
+// set loop with a grid
+// Grid is made with a vector of a vector of strings (3x5, 5 columns for | symbols on index 0, 2, 4)
+
+// make logic for seeing three in a row
+
+    return 0;
+}
